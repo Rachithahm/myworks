3e6bc6d039542afaba65f02e389f727c3a08e098
Rachithahm <113096705+Rachithahm@users.noreply.github.com>
2022-09-08 15:35:41 +0530

    Add files via upload

---
+++ bhima/fontawesome-free/right.sidebar.js
@@ -0,0 +1,80 @@
+/**
+ *
+ * ------------------
+ * You should not use this file in production.
+ * This file is for demo purposes only.
+ */
+(function ($) {
+  'use strict'
+
+  var $sidebar   = $('.control-sidebar')
+  var $container = $('<div />', {
+    class: 'p-3 control-sidebar-content'
+  })
+
+  $sidebar.append($container)
+
+  var navbar_dark_skins = [
+    'navbar-primary',
+    'navbar-secondary',
+    'navbar-info',
+    'navbar-success',
+    'navbar-danger',
+    'navbar-indigo',
+    'navbar-purple',
+    'navbar-pink',
+    'navbar-navy',
+    'navbar-lightblue',
+    'navbar-teal',
+    'navbar-cyan',
+    'navbar-dark',
+    'navbar-gray-dark',
+    'navbar-gray',
+  ]
+
+  var navbar_light_skins = [
+    'navbar-light',
+    'navbar-warning',
+    'navbar-white',
+    'navbar-orange',
+  ]
+
+  var $no_border_checkbox = $('<a href="#"><i class="fa fa-cog"/></a>', {
+    type   : 'icon',
+    value  : 1,
+    checked: $('.main-header').hasClass('border-bottom-0'),
+    'class': 'mr-1'
+  }).on('click', function () {
+    if ($(this).is(':checked')) {
+      $('.main-header').addClass('border-bottom-0')
+    } else {
+      $('.main-header').removeClass('border-bottom-0')
+    }
+  })
+
+  var $no_border_container = $('<div />', {'class': 'mb-1'}).append($no_border_checkbox).append('<a href="#"><span>&nbsp;Settings</span></a>')
+  $container.append($no_border_container)
+
+  var $no_border_checkbox = $('<a href="#"><i class="fa fa-question-circle"/></a>', {
+    type   : 'icon',
+    value  : 1,
+    checked: $('.main-header').hasClass('border-bottom-0'),
+    'class': 'mr-1'
+  }).on('click', function () {
+    if ($(this).is(':checked')) {
+      $('.main-header').addClass('border-bottom-0')
+    } else {
+      $('.main-header').removeClass('border-bottom-0')
+    }
+  })
+  var $no_border_container = $('<div />', {'class': 'mb-1'}).append($no_border_checkbox).append('<a href="#"><span>&nbsp;Help</span></a>')
+  $container.append($no_border_container)
+
+
+  $('.product-image-thumb').on('click', function() {
+    const image_element = $(this).find('img');
+    $('.product-image').prop('src', $(image_element).attr('src'))
+    $('.product-image-thumb.active').removeClass('active');
+    $(this).addClass('active');
+  });
+})(jQuery)
